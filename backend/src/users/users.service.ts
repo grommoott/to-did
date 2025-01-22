8aa7f5ba625e46f4db4e80cc0a941cd048252eb2
grommoott <grommoott@gmail.com>
2025-01-22 18:25:28 +0700

    backend: add createUser() into UsersService

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { User } from "./entity/user.entity";
+import { Repository } from "typeorm";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { HashService } from "src/helpers/hash/hash.service";
 
 @Injectable()
-export class UsersService { }
+export class UsersService {
+    constructor(@InjectRepository(User) private readonly userRepository: Repository<User>, private readonly hashService: HashService) { }
+
+    async createUser(createUserDto: CreateUserDto): Promise<User> {
+        const user = new User()
+        user.username = user.username;
+        user.passwordHash = await this.hashService.hash(createUserDto.password)
+        return this.userRepository.save(user)
+    }
+}
