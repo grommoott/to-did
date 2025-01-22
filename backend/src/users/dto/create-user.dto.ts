12c0d7ad59053c9326b9e5d661bf092882f4c2b2
grommoott <grommoott@gmail.com>
2025-01-22 18:08:15 +0700

    backend: add CreateUserDto

---
+++ backend/src/users/dto/create-user.dto.ts
@@ -0,0 +1,13 @@
+import { IsNotEmpty, MaxLength, MinLength } from "class-validator";
+
+export class CreateUserDto {
+    @IsNotEmpty()
+    @MinLength(3)
+    @MaxLength(30)
+    username: string;
+
+    @IsNotEmpty()
+    @MinLength(4)
+    @MaxLength(128)
+    password: string;
+}
