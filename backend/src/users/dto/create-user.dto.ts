f559dd55427c94883f8243a914219050a3a208e8
grommoott <grommoott@gmail.com>
2025-01-23 21:31:20 +0700

    backend: remove semicolons in CreateUserDto

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -4,10 +4,10 @@ export class CreateUserDto {
     @IsNotEmpty()
     @MinLength(3)
     @MaxLength(30)
-    username: string;
+    username: string
 
     @IsNotEmpty()
     @MinLength(4)
     @MaxLength(128)
-    password: string;
+    password: string
 }
