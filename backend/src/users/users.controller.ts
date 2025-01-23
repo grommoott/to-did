1bae801738ce283bd375cfafc7356d208c8e5278
grommoott <grommoott@gmail.com>
2025-01-23 21:31:36 +0700

    backend: add users/get() request

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UsersService } from "./users.service";
+import { IUser } from "./interfaces/user.interface";
 
 @Controller("users")
 export class UsersController {
@@ -9,5 +10,10 @@ export class UsersController {
     @Post()
     async create(@Body() createUserDto: CreateUserDto) {
         this.usersService.createUser(createUserDto)
+    }
+
+    @Get(":id")
+    async get(@Param("id") id: string): Promise<IUser> {
+        return this.usersService.getUser(Number(id))
     }
 }
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { User } from "./entity/user.entity";
 import { Repository } from "typeorm";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { HashService } from "src/helpers/hash/hash.service";
+import { GetUserDto } from "./dto/get-user.dto";
+import { IUser } from "./interfaces/user.interface";
 
 @Injectable()
 export class UsersService {
@@ -11,8 +13,18 @@ export class UsersService {
 
     async createUser(createUserDto: CreateUserDto): Promise<User> {
         const user = new User()
-        user.username = createUserDto.username;
+        user.username = createUserDto.username
         user.passwordHash = await this.hashService.hash(createUserDto.password)
         return this.userRepository.save(user)
+    }
+
+    async getUser(id: number): Promise<IUser> {
+        const user = await this.userRepository.findOneBy({ id: id })
+
+        if (user == null) {
+            throw new NotFoundException()
+        }
+
+        return user
     }
 }
