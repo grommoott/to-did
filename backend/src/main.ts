0fd7af47986a408ce91f559566a09ea97757838a
grommoott <grommoott@gmail.com>
2025-01-23 14:20:02 +0700

    backend: add cookieParser middleware

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
     config()
     const app = await NestFactory.create(AppModule)
+    app.use(cookieParser())
     await app.listen(process.env.PORT ?? 7777)
 }
 bootstrap();
