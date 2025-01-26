fec0109112e9944a9c092c7d1ef2dd989970c826
grommoott <grommoott@gmail.com>
2025-01-26 12:08:01 +0700

    backend: add cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
 import * as cookieParser from 'cookie-parser';
+import * as cors from "cors"
 
 async function bootstrap() {
     config()
     const app = await NestFactory.create(AppModule)
     app.use(cookieParser())
+    app.use(cors())
     await app.listen(process.env.PORT ?? 7777)
 }
 bootstrap();
