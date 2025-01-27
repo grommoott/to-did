b58e62f0892f515be3ff1fba336c2f571363ca09
grommoott <grommoott@gmail.com>
2025-01-27 21:14:26 +0700

    frontend: add toDoSteps 2,3,4

--- frontend/src/app/toDoSteps.ts
+++ frontend/src/app/toDoSteps.ts
@@ -1,5 +1,6 @@
 interface ToDo {
     type: "imgLeft" | "imgRight"
+    transformOriginClass: "left-top" | "right-top" | "center" | "to-do-3"
     imagePath: string
     title: string
     content: string
@@ -8,9 +9,31 @@ interface ToDo {
 const toDoSteps: ToDo[] = [
     {
         type: "imgLeft",
+        transformOriginClass: "right-top",
         imagePath: "/toDoStep1.png",
         title: "Шаг 1",
         content: "Зарегистрируйтесь или войдите в свой аккаунт To Did"
+    },
+    {
+        type: "imgRight",
+        transformOriginClass: "center",
+        imagePath: "/toDoStep2.png",
+        title: "Шаг 2",
+        content: "Нажмите кнопку \"Зарегистрироваться\", чтобы создать аккаунт"
+    },
+    {
+        type: "imgLeft",
+        transformOriginClass: "to-do-3",
+        imagePath: "/toDoStep3.png",
+        title: "Шаг 3",
+        content: "Напишите свой туду и нажмите кнопку \"Создать Туду\""
+    },
+    {
+        type: "imgRight",
+        transformOriginClass: "center",
+        imagePath: "/toDoStep4.png",
+        title: "Готово!",
+        content: "Теперь вы самый продуктивный, умный и просто хороший человек"
     }
 ]
 
