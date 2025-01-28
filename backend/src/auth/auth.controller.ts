6ce939c59d37f49a005bb542619bec8a3987af4f
grommoott <grommoott@gmail.com>
2025-01-28 15:42:13 +0700

    backend: fix auth/refresh()

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -2,14 +2,19 @@ import { Body, Controller, Post, Req, Res, UseGuards } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { LoginDto } from "./dto/login.dto";
 import { CookieOptions, Request, Response } from "express";
-import { AuthGuard } from "./auth.guard";
 
 @Controller("auth")
 export class AuthController {
     constructor(private readonly authService: AuthService) { }
 
-    private cookieOptions: CookieOptions = {
+    private accessTokenOptions: CookieOptions = {
         maxAge: 1200_000,
+        httpOnly: true,
+        sameSite: "strict"
+    }
+
+    private refreshTokenOptions: CookieOptions = {
+        maxAge: 15 * 24 * 3600_000,
         httpOnly: true,
         sameSite: "strict"
     }
@@ -18,17 +23,16 @@ export class AuthController {
     async login(@Body() loginDto: LoginDto, @Res({ passthrough: true }) res: Response) {
         const tokens = await this.authService.login(loginDto)
 
-        res.cookie("Access-Token", tokens.access, this.cookieOptions)
-        res.cookie("Refresh-Token", tokens.refresh, { ...this.cookieOptions, path: "/auth/refresh" })
+        res.cookie("Access-Token", tokens.access, this.accessTokenOptions)
+        res.cookie("Refresh-Token", tokens.refresh, { ...this.refreshTokenOptions, path: "/auth/refresh" })
     }
 
-    @UseGuards(AuthGuard)
     @Post("refresh")
     async refresh(@Req() req: Request, @Res({ passthrough: true }) res: Response) {
         const tokens = await this.authService.refresh(req.cookies["Refresh-Token"])
 
-        res.cookie("Access-Token", tokens.access, this.cookieOptions)
-        res.cookie("Refresh-Token", tokens.refresh, { ...this.cookieOptions, path: "/auth/refresh" })
+        res.cookie("Access-Token", tokens.access, this.accessTokenOptions)
+        res.cookie("Refresh-Token", tokens.refresh, { ...this.refreshTokenOptions, path: "/auth/refresh" })
     }
 
     @Post("logout")
